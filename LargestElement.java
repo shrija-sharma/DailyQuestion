c1cd5755fc7302ffc763f2e2fd8f48fecae0fae2
chikkisharma <shrija.sharma2004@gmail.com>
2025-07-14 19:09:54 +0530

    Largest Element in an array (GFG)

---
+++ LargestElement.java
@@ -0,0 +1,13 @@
+public class LargestElement {
+      public static int largest(int[] arr) {
+        // code here
+        int n=arr.length;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
