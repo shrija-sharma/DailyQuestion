a3c1cf173c773891968f711c2b936afbafbc047f
chikkisharma <shrija.sharma2004@gmail.com>
2025-07-16 20:57:51 +0530

    Rotate Array to the left by K steps (code360)

---
+++ RotateArray1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class RotateArray1 {
+      public static void reverse(ArrayList<Integer> arr,int i,int j){
+        while(i<j){
+            int temp=arr.get(i);
+            arr.set(i,arr.get(j));
+            arr.set(j,temp);
+            i++;
+            j--;
+        }
+    }
+	public static ArrayList<Integer> rotateArray(ArrayList<Integer> arr, int k) {
+        // Write your code here.
+     //   ArrayList<Integer> ans=new ArrayList<>();
+        int n=arr.size();
+        k=k%n;
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        reverse(arr,0,n-1);
+        return arr;
+    }
+}
