ba7d77736cdf7f42cbc2ef7141860190525d0785
chikkisharma <shrija.sharma2004@gmail.com>
2025-07-17 18:46:43 +0530

    Implemented Linear Search

---
+++ ArraySearch.java
@@ -0,0 +1,12 @@
+public class ArraySearch {
+      static int search(int arr[], int x) {
+        // code here
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
