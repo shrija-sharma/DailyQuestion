0137987f94b37f17343265308cada1ab28e3391a
chikkisharma <shrija.sharma2004@gmail.com>
2025-07-23 18:55:56 +0530

    find the nth fibonacci number with space optimization

---
+++ FibonacciNumber3.java
@@ -0,0 +1,11 @@
+public class FibonacciNumber3 {
+  public int fib(int n) {
+    int a = 0, b = 0, c = 1;
+    for (int i = 0; i <= n; i++) {
+      a = b;
+      b = c;
+      c = a + b;
+    }
+    return a;
+  }
+}
