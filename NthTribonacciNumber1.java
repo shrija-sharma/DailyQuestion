cec37d7fc9504e338790bc63f3fa1f8e53cc8f9c
chikkisharma <shrija.sharma2004@gmail.com>
2025-07-25 13:47:16 +0530

    find nth tribonacci number using DP tabulation method

---
+++ NthTribonacciNumber1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class NthTribonacciNumber1 {
+      public int tabulation(int n,int[] dp){
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        return tabulation(n,dp);
+    }
+}
