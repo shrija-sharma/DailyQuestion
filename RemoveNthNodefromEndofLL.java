69d21527a9735f8705b9966a90e7a17746cc2f4a
chikkisharma <shrija.sharma2004@gmail.com>
2025-07-28 17:23:29 +0530

    Remove nth node from end of Linked List

---
+++ RemoveNthNodefromEndofLL.java
@@ -0,0 +1,20 @@
+public class RemoveNthNodefromEndofLL {
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode slow = head;
+    ListNode fast = head;
+    for (int i = 0; i < n; i++) {
+      fast = fast.next;
+    }
+    if (fast == null) {
+      head = head.next;
+      return head;
+    }
+    while (fast.next != null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+  }
+
+}
