7897251ab5be4a0785595dc3dcbdcb1e97d50406
chikkisharma <shrija.sharma2004@gmail.com>
2025-07-31 20:39:53 +0530

    Binary Search (it reduce the search space)

---
+++ BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+  public int search(int[] nums, int target) {
+    int n = nums.length;
+    int s = 0, e = n - 1;
+    while (s <= e) {
+      int mid = s + (e - s) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        s = mid + 1;
+      } else if (nums[mid] > target) {
+        e = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
