aa6195f21fdc59d0fd23fb99b55b0a58033bb4b6
chikkisharma <shrija.sharma2004@gmail.com>
2025-08-07 21:03:32 +0530

    Insert of a given node in Binary Search Tree

---
+++ InsertaintoaBST.java
@@ -0,0 +1,45 @@
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+public class InsertaintoaBST {
+  public TreeNode insertIntoBST(TreeNode root, int val) {
+    if (root == null) {
+      return new TreeNode(val);
+    }
+    TreeNode curr = root;
+    while (true) {
+      if (curr.val <= val) {
+        if (curr.right != null)
+          curr = curr.right;
+        else {
+          curr.right = new TreeNode(val);
+          break;
+        }
+      } else {
+        if (curr.left != null)
+          curr = curr.left;
+        else {
+          curr.left = new TreeNode(val);
+          break;
+        }
+      }
+    }
+    return root;
+  }
+}
