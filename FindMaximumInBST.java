94d051697dae126ed8e7f31ea6dbacdbcc3adae4
chikkisharma <shrija.sharma2004@gmail.com>
2025-08-07 19:23:31 +0530

    Subarray sum equals K

--- FindMaximumInBST.java
+++ FindMaximumInBST.java
@@ -1,4 +1,16 @@
 public class FindMaximumInBST {
+     class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
   public static int findMax(TreeNode root) {
     if (root == null) {
       return -1;
--- FindMinimumInBST.java
+++ FindMinimumInBST.java
@@ -1,4 +1,16 @@
 import java.util.*;
+   class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 public class FindMinimumInBST {
     public static int findMin(TreeNode root){
         if(root==null){
