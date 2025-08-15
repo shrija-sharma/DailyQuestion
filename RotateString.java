553068b3b5473cf19943688c8d21fe924d89f318
chikkisharma <shrija.sharma2004@gmail.com>
2025-08-15 23:38:49 +0530

    Rotate String

---
+++ RotateString.java
@@ -0,0 +1,12 @@
+public class RotateString {
+  public boolean rotateString(String s, String goal) {
+    if (s.length() != goal.length()) {
+      return false;
+    }
+    String str = s + s;
+    if (str.contains(goal)) {
+      return true;
+    }
+    return false;
+  }
+}
